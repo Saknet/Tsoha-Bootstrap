73e29b62ac854bb648f57eaa9c9567576ba6c465
Andra Andreas <andra@da3-cs-dk108-01.pc.helsinki.fi>
2017-07-29 01:01:54 +0300

    tietokanta juttuja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Person (name, password, admin) VALUES ('Ope1', 'Ope1', TRUE);
+INSERT INTO Person (name, password) VALUES ('Ope2', 'Ope2');
+
+INSERT INTO Topic (name, description, courseName) VALUES ('Shakki', 'yksinkertainen shakki', 'Ohjelmointi');
+INSERT INTO Topic (name, description, courseName) VALUES ('Työaihekanta', 'Harjoitustöiden aiheiden tietokanta', 'Tietokantasovellus');
+INSERT INTO Topic (name, description, courseName) VALUES ('Tiedon tiivistys', 'Tiedoston pakkausta eri algoritmeilla', 'Tietorakenteet ja algoritmit');
+
+INSERT INTO Credit (interupted, startdate, grade) VALUES (TRUE, '2014-11-11', 0);
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,25 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Person(
+  id SERIAL PRIMARY KEY, 
+  name varchar(50) NOT NULL, 
+  password varchar(50) NOT NULL,
+  admin boolean DEFAULT FALSE
+);
+
+CREATE TABLE Topic(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL, 
+  addedBy INTEGER REFERENCES Person(id),
+  description varchar(500),
+  courseName varchar(50)
+);
+
+CREATE TABLE Credit(
+  id SERIAL PRIMARY KEY, 
+  givenBy INTEGER REFERENCES Person(id),
+  topic_id INTEGER REFERENCES Topic(id),
+  interrupted boolean DEFAULT FALSE,
+  startDate DATE,
+  endDate DATE,
+  grade INTEGER
+);
